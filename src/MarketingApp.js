3ee09c2b22fc53ffbd48a99af2a8fd91c8d036e1
Chirag Kalwani <chirag.kalwani@wealthtech.ai>
2026-06-22 19:04:37 +0530

    updated

--- src/MarketingApp.js
+++ src/MarketingApp.js
@@ -64,7 +64,7 @@ export default function MarketingApp() {
             <Route path="/portfolio-management-services" element={<PmsMarketingPage />} />
             <Route path="/itr-filing" element={<ItrLandingPage />} />
             <Route path="/itr-filing-for-employee" element={<ItrFilingForEmployee />} />
-            <Route path="/itr-filing-for-employees" element={<ItrFilingForEmployee />} />
+            <Route path="/itr-filing2" element={<ItrFilingForEmployee />} />
           </Routes>
         </Suspense>
       </HelmetProvider>
